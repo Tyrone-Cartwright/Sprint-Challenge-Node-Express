978d7cd730b527d94b20b6bcd1faf1f8046d77d8
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-11 13:50:25 -0500

    Implemented PUT and DELETE for projectsRouter file MVP met

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const actionsRouter = require("../actions/actionsRouter.js");
-// const projectsRouter = require("../projects/projectsRouter.js");
+const projectsRouter = require("../projects/projectsRouter.js");
 
 const router = express.Router();
 
 router.use("/actions", actionsRouter);
-// router.use("/projects", projectsRouter);
+router.use("/projects", projectsRouter);
 
 module.exports = router;
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const projectsdb = require("../projects/projectsRouter.js");
+const projectsdb = require("../data/helpers/projectModel");
 
 router.get("/", (req, res) => {
   projectsdb
     .get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The project could not be retrieved." });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  projectsdb
+    .get(req.params.id)
     .then(projects => {
       res.status(200).json(projects);
     })
@@ -22,9 +33,48 @@ router.post("/", (req, res) => {
         res.status(200).json(projects);
       })
       .catch(err => {
-        res.status(500).json({ message: "Project could not be posted." });
+        res
+          .status(500)
+          .json({ message: "Project could not be posted.", eror: err });
       });
   }
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, description, completed } = req.body;
+  projectsdb
+    .update(id, { name, description, completed })
+    .then(updated => {
+      if (!updated) {
+        res.status(400).json({
+          error: `The project post with Id ${id} could not be updated.`
+        });
+      } else {
+        res.status(200).json(updated);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: `There was a problem saving project post ${id}` });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  projectsdb
+    .remove(id)
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          message: `The project with specified Id ${id} could not be deleted`
+        });
+    });
 });
 
 module.exports = router;
