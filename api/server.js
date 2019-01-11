064b584bbf6abea1f5b7e7e5ebf6204af95d462a
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-11 11:46:51 -0500

    Added api/server file, config/mware file, index.js also added helmet cors morgan

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const configMiddleware = require("../config/middleware.js");
+
+const server = express();
+
+configMiddleware(server);
+
+module.exports = server;
